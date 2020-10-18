bd68c9d0cf881e20dc74113402f0e55c9501a188
Ben Baron <ben@einsteinx2.com>
2020-10-17 22:36:25 -0500

    Suppressed warnings in main framework header

--- TagLibXcode/TagLibXcode.h
+++ TagLibXcode/TagLibXcode.h
@@ -15,7 +15,13 @@ FOUNDATION_EXPORT const unsigned char TagLibXcodeVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <TagLibXcode/PublicHeader.h>
 
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wquoted-include-in-framework-header"
+#pragma clang diagnostic ignored "-Wdocumentation"
+#pragma clang diagnostic ignored "-Wdocumentation-deprecated-sync"
 
 #import <TagLibXcode/fileref.h>
 #import <TagLibXcode/tag.h>
 #import <TagLibXcode/tpropertymap.h>
+
+#pragma clang diagnostic pop
