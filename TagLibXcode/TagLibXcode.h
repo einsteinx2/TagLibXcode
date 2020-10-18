cb891eac8e959ce0412754fa099eca7ea91a37b7
Ben Baron <ben@einsteinx2.com>
2020-10-17 21:15:59 -0500

    Compiles for iOS and Mac but not tested yet

--- TagLibXcode/TagLibXcode.h
+++ TagLibXcode/TagLibXcode.h
@@ -16,3 +16,6 @@ FOUNDATION_EXPORT const unsigned char TagLibXcodeVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <TagLibXcode/PublicHeader.h>
 
 
+#import <TagLibXcode/fileref.h>
+#import <TagLibXcode/tag.h>
+#import <TagLibXcode/tpropertymap.h>
---
+++ TagLibXcode/taglib_config.h
@@ -0,0 +1,12 @@
+//
+//  taglib_config.h
+//  TagLibXcode
+//
+//  Created by Benjamin Baron on 10/17/20.
+//
+
+#ifndef taglib_config_h
+#define taglib_config_h
+
+
+#endif /* taglib_config_h */
