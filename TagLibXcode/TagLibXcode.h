b73b9541e71a8b7f884335c6ba867458bd91b931
Ben Baron <ben@einsteinx2.com>
2020-10-17 20:53:09 -0500

    Initial Commit

---
+++ TagLibXcode/TagLibXcode.h
@@ -0,0 +1,18 @@
+//
+//  TagLibXcode.h
+//  TagLibXcode
+//
+//  Created by Benjamin Baron on 10/17/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TagLibXcode.
+FOUNDATION_EXPORT double TagLibXcodeVersionNumber;
+
+//! Project version string for TagLibXcode.
+FOUNDATION_EXPORT const unsigned char TagLibXcodeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TagLibXcode/PublicHeader.h>
+
+
